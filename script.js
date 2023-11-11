3e0aabfc86801164a98c946e9c09c64357482090
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-11 13:51:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function isSameType(value1, value2) {
   //your js code here
 
-	return typeof value1 === typeof value2;     
+	return typeof value1 == typeof value2;     
 }
 
 // do not change the code below.
