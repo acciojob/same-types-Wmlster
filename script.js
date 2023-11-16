51f1e9a28c6f7138ed5be9562190e1e0066ced38
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-16 18:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function isSameType(value1, value2) {
   //your js code here
+	if(value1>0)   
+	{
+		value1=Number(value1);
+	}
+	if(value2>0)
+	{
+		value2=Number(value2);
+	}
+	
 	let y1=typeof value1;
 	let y2=typeof value2;
 
